4b71e8cf4885599020e7471e7c836947d2d0f46a
rosecodez <rosebeats09@gmail.com>
2024-08-10 16:39:15 -0400

    Set up passportJS

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+require("dotenv").config();
 
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -19,8 +31,56 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+passport.use(async (username, password, done) => {
+  try {
+    const user = await prisma.user.findFirst({ where: { username } });
+
+    if (!user) {
+      return done(null, false, { message: "Incorrect username" });
+    }
+    if (user.password !== password) {
+      return done(null, false, { message: "Incorrect password" });
+    }
+    return done(null, user);
+  } catch (error) {
+    return done(err);
+  }
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findFirst({ where: { id } });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
